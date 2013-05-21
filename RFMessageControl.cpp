5f94e2e9911fcce5f1acb5975569b7f3c5ac851e
Bas Mesman <captainf@dds.nl>
2013-05-21 23:18:33 +0200

    Implementation of MessageQueueItem::init

--- RFMessageControl.cpp
+++ RFMessageControl.cpp
@@ -1,2 +1,25 @@
 #include "RFMessageControl.h"
+#include "Arduino.h"
 
+MessageQueueItem::MessageQueueItem()
+{
+  m_messageType = ACKNOWLEDGE;
+  m_channel = 0;
+  m_messageId = 0;
+  m_retriesLeft = 0;
+}
+
+void MessageQueueItem::init(uint8_t channel, uint8_t messageId, uint8_t * message, uint8_t messageLength)
+{
+  m_channel = channel;
+  m_messageId = messageId;
+  m_retriesLeft = MAXRETRIES;
+  
+  int i;
+  for (i=0; i < MESSAGE_BUFFER_SIZE; i++)
+  {
+     m_messageBuffer[i] = 0;
+  }
+  
+  memcpy(m_messageBuffer, message, min(MESSAGE_BUFFER_SIZE, messageLength)); 
+}
