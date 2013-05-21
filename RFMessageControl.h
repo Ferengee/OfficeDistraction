531657edd2cfdd62ff4df6e6d7330cecd8df0d53
Bas Mesman <captainf@dds.nl>
2013-05-21 23:18:08 +0200

    Made all byte types uint_8

--- RFMessageControl.h
+++ RFMessageControl.h
@@ -6,31 +6,33 @@
 #define MAXMESSAGECOUNT 16
 #define MAXRETRIES 3
 
+#define MESSAGE_BUFFER_SIZE VW_MAX_PAYLOAD-4
+
 #define MESSAGE 0
 #define ACKNOWLEDGE 1
 #define CONFIRM 2
 
-typedef void (* MessageReceivedEventHandler) (byte channel, uint8_t * message);
+typedef void (* MessageReceivedEventHandler) (uint8_t channel, uint8_t * message, uint8_t messageLength);
 
 class MessageQueueItem
 {
 public:
   MessageQueueItem();
-  void init (byte channel, uint8_t * message, byte messageId);
+  void init (uint8_t channel, uint8_t messageId, uint8_t * message, uint8_t messageLength);
   uint8_t * getMessage();
-  byte getChannel();
-  byte getRetriesLeft();
+  uint8_t getChannel();
+  uint8_t getRetriesLeft();
   void decrementRetriesLeft();
   
 private:
   
-  byte m_messageType;
-  byte m_channel;
-  byte m_messageId;
+  uint8_t m_messageType;
+  uint8_t m_channel;
+  uint8_t m_messageId;
   
-  byte m_retriesLeft;
+  uint8_t m_retriesLeft;
 
-  uint8_t m_messageBuffer[VW_MAX_PAYLOAD - 4*sizeof(byte)]; // about 30 - (3 + 4) = 23 bytes
+  uint8_t m_messageBuffer[MESSAGE_BUFFER_SIZE]; // about 30 - (3 + 4) = 23 bytes
 };
 
 class RFMessageControl
@@ -42,9 +44,9 @@ public:
    * writes max sizeof m_messageBuffer bytes
    * returns false if the queue is full 
    */
-  bool sendMessage(byte channel, uint8_t * message, byte messageLength);
+  bool sendMessage(uint8_t channel, uint8_t * message, uint8_t messageLength);
   /* m_retriesLeft is set to 0 */
-  void acknowledge(byte messageId);
+  void acknowledge(uint8_t messageId);
   /* 
    * iterate queue, 
    * send all messages with  m_retriesLeft > 0
@@ -64,7 +66,7 @@ private:
    * to be used in sendMessage
    */
   MessageQueueItem * getUnusedMessage();
-  byte m_lastMessageId;
+  uint8_t m_lastMessageId;
   MessageReceivedEventHandler callback;
 };
 
