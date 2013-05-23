283a27a84c8bd5dd3d9ee2a7040f80c28e20050a
Bas Mesman <captainf@dds.nl>
2013-05-23 23:38:18 +0200

    Changed the MessageQueueItem class to store its properties directly into its buffer started with implementation Using MessageQueueItem for outgoing as well as incomming messages

--- RFMessageControl.cpp
+++ RFMessageControl.cpp
@@ -3,23 +3,187 @@
 
 MessageQueueItem::MessageQueueItem()
 {
-  m_messageType = ACKNOWLEDGE;
-  m_channel = 0;
-  m_messageId = 0;
-  m_retriesLeft = 0;
 }
 
+
+void MessageQueueItem::setMessageType(uint8_t messageType){
+  m_messageBuffer[0] = messageType;
+}
+
+uint8_t MessageQueueItem::getMessageType(){
+  return m_messageBuffer[0];
+}
+
+void MessageQueueItem::setChannel(uint8_t channel){
+  m_messageBuffer[1] = channel;
+}
+void MessageQueueItem::setMessageId(uint8_t messageId){
+  m_messageBuffer[2] = messageId;
+}
+uint8_t MessageQueueItem::getMessageId(){
+  return m_messageBuffer[2];
+}
+
+void MessageQueueItem::setRetriesLeft(uint8_t retries){
+  m_messageBuffer[3] = retries;
+}
+
+uint8_t * MessageQueueItem::getBuffer(){
+  return m_messageBuffer;
+}
+
+void MessageQueueItem::getMessage(uint8_t * message, uint8_t * length){
+  memcpy(message, m_messageBuffer + MESSAGE_HEADER_LENGTH,min(MESSAGE_BUFFER_SIZE, *length));
+}
+
+
+void MessageQueueItem::decrementRetriesLeft()
+{
+   m_messageBuffer[3]--;
+}
 void MessageQueueItem::init(uint8_t channel, uint8_t messageId, uint8_t * message, uint8_t messageLength)
 {
-  m_channel = channel;
-  m_messageId = messageId;
-  m_retriesLeft = MAXRETRIES;
+  setMessageType(MESSAGE);
+  setChannel(channel);
+  setMessageId(messageId);
+  setRetriesLeft(MAXRETRIES);
   
   int i;
-  for (i=0; i < MESSAGE_BUFFER_SIZE; i++)
+  for (i=MESSAGE_HEADER_LENGTH; i < MESSAGE_BUFFER_SIZE + MESSAGE_HEADER_LENGTH; i++)
   {
      m_messageBuffer[i] = 0;
   }
   
-  memcpy(m_messageBuffer, message, min(MESSAGE_BUFFER_SIZE, messageLength)); 
+  memcpy(m_messageBuffer + MESSAGE_HEADER_LENGTH, message, min(MESSAGE_BUFFER_SIZE, messageLength)); 
+}
+
+void MessageQueueItem::destroy()
+{
+  setRetriesLeft(0);
+}
+
+void MessageQueueItem::acknowledge(uint8_t acknowledgementType)
+{
+  if(getMessageType() == MESSAGE && acknowledgementType == ACKNOWLEDGE){
+    setMessageType(CONFIRM);
+    setRetriesLeft(MAXRETRIES);
+  } else if(getMessageType() == CONFIRM && acknowledgementType == ACKNOWLEDGE_CONFIRM) {
+    destroy();
+  }
+}
+
+RFMessageControl::RFMessageControl(int txPin, int rxPin, int speed)
+{
+  vw_set_tx_pin ((uint8_t)txPin);
+  vw_set_rx_pin ((uint8_t)rxPin);
+  vw_set_ptt_inverted(true);
+  vw_setup ((uint16_t)speed);
+  vw_rx_start();   
+}
+
+bool RFMessageControl::sendMessage(uint8_t channel, uint8_t * message, uint8_t messageLength){
+  MessageQueueItem * item;
+  bool succes = getUnusedMessage(item, m_sending);
+  if (succes)
+  {
+    item->init(channel, m_lastMessageId++, message, messageLength);
+  }
+  return succes;
+}
+
+bool RFMessageControl::getUnusedMessage(MessageQueueItem * item, MessageQueueItem * queue){
+  findMessage(-1, -1, 0, item, queue);
+}
+
+void RFMessageControl::acknowledge(MessageQueueItem * acknowledgement){
+  MessageQueueItem * item;
+  if(findMessage(acknowledgement->getChannel(), acknowledgement->getMessageId(), -1, item, m_sending)){
+    // only acknowledge if type == MESSAGE and other.type == ACKNOWLEDGE
+    //                  or type == CONFIRM and other.type == ACKNOWLEDGE_CONFIRM
+    item->acknowledge(acknowledgement->getMessageType());
+  }
+}
+
+bool MessageChannelEquals(MessageQueueItem * item, uint8_t channel)
+{
+  return item->getChannel() == channel;
+}
+
+bool MessageMessageIdEquals(MessageQueueItem * item, uint8_t messageId)
+{
+  return item->getMessageId() == messageId;
+}
+
+bool MessageRetriesLeftEquals(MessageQueueItem * item, uint8_t retriesLeft)
+{
+  return item->getRetriesLeft() == retriesLeft;
+}
+
+bool RFMessageControl::findMessage(int channel, int messageId, int retriesLeft, MessageQueueItem * item, MessageQueueItem * queue)
+{
+  MessageParameterEquals testFunctions[3];
+  uint8_t values[3];
+  int testCount = 0;
+  
+  if(channel != -1){
+    values[testCount] = (uint8_t)channel;
+    testFunctions[testCount] = MessageChannelEquals;
+    testCount++;
+  }
+  if(messageId != -1){
+    values[testCount] = (uint8_t)messageId;
+    testFunctions[testCount] = MessageMessageIdEquals;
+    testCount++;
+  }
+  if(retriesLeft != -1){
+    values[testCount] = (uint8_t)retriesLeft;
+    testFunctions[testCount] = MessageRetriesLeftEquals;
+    testCount++;
+  }
+    
+  MessageQueueItem * candidate;
+  int i, f;
+  bool found;
+  
+  for (i=0; i < MAXMESSAGECOUNT; i++)
+  {
+    candidate = &queue[i];
+    found = true;
+    for(f=0; f < testCount; f++){
+      found = testFunctions[f](candidate, values[f]);
+      if (!found){
+        break;
+      }
+    }
+    if(found){
+      item = candidate;
+      break; 
+    }
+  }
+  return found;
+}
+
+void RFMessageControl::sendRemainingMessages(){
+  MessageQueueItem * item;
+  int i;
+  for (i=0; i < MAXMESSAGECOUNT; i++)
+  {
+     item = &m_sending[i];
+     if (item->getRetriesLeft() > 0){
+       item->decrementRetriesLeft();
+       send(item);
+     }
+  }
+}
+/* sent full buffer for now
+ * optimize to send only relevant data later
+ */
+void send(MessageQueueItem * item){
+  uint8_t length = VW_MAX_PAYLOAD;
+  vw_send(item->getBuffer(),length);
+}
+void RFMessageControl::handleIncommingMessages(){}
+void RFMessageControl::update(){
+  sendRemainingMessages();
+  handleIncommingMessages();
 }
--- RFMessageControl.h
+++ RFMessageControl.h
@@ -5,57 +5,86 @@
 #include "../VirtualWire/VirtualWire.h"
 #define MAXMESSAGECOUNT 16
 #define MAXRETRIES 3
-
-#define MESSAGE_BUFFER_SIZE VW_MAX_PAYLOAD-4
+#define MESSAGE_HEADER_LENGTH 4
+#define MESSAGE_BUFFER_SIZE VW_MAX_PAYLOAD-MESSAGE_HEADER_LENGTH
 
 #define MESSAGE 0
 #define ACKNOWLEDGE 1
 #define CONFIRM 2
+#define ACKNOWLEDGE_CONFIRM 3
 
 typedef void (* MessageReceivedEventHandler) (uint8_t channel, uint8_t * message, uint8_t messageLength);
+
 
 class MessageQueueItem
 {
 public:
   MessageQueueItem();
   void init (uint8_t channel, uint8_t messageId, uint8_t * message, uint8_t messageLength);
-  uint8_t * getMessage();
+  void destroy();
+  
+  void getMessage(uint8_t * message, uint8_t * length);
+  
+  uint8_t * getBuffer();
+  uint8_t getMessageId();
+
   uint8_t getChannel();
   uint8_t getRetriesLeft();
+  
+  uint8_t getMessageType();
+  
   void decrementRetriesLeft();
+  void acknowledge(uint8_t acknowledgementType);
   
-private:
+private:  
+  void setMessageType(uint8_t messageType);
   
-  uint8_t m_messageType;
-  uint8_t m_channel;
-  uint8_t m_messageId;
-  
-  uint8_t m_retriesLeft;
 
-  uint8_t m_messageBuffer[MESSAGE_BUFFER_SIZE]; // about 30 - (3 + 4) = 23 bytes
+  void setChannel(uint8_t channel);
+  void setMessageId(uint8_t messageId);
+  
+  void setRetriesLeft(uint8_t retries);
+
+  uint8_t m_messageBuffer[VW_MAX_PAYLOAD]; // about 30 - (3 ) = 27 bytes
 };
 
+typedef bool (* MessageParameterEquals) (MessageQueueItem * item, uint8_t value);
+
+/*
+ * send messages via sendMessage()
+ * receive messages via the MessageReceivedEventHandler callback
+ * trigger processing of messages in the sending queue via update()
+ * which has to be called or periodically, with out it no messages get sent, sendMessages only
+ * puts messages in the queue
+ */
 class RFMessageControl
 {
 public:
-  RFMessageControl();
+  RFMessageControl(int txPin, int rxPin, int speed);
   /* 
    * Copies the contents of the message
    * writes max sizeof m_messageBuffer bytes
    * returns false if the queue is full 
+   * 
    */
   bool sendMessage(uint8_t channel, uint8_t * message, uint8_t messageLength);
-  /* m_retriesLeft is set to 0 */
-  void acknowledge(uint8_t messageId);
+  void acknowledge(MessageQueueItem * acknowledgement);
   /* 
    * iterate queue, 
    * send all messages with  m_retriesLeft > 0
    */
   void sendRemainingMessages();
+  void handleIncommingMessages();
+  /*
+   * sendRemainingMessages()
+   * handleIncommingMessages()
+   */
+  void update();
   void setMessageReceivedEventHandler(MessageReceivedEventHandler eh);
 
 private:
-  MessageQueueItem m_queue[MAXMESSAGECOUNT];
+  MessageQueueItem m_sending[MAXMESSAGECOUNT];
+  MessageQueueItem m_received[MAXMESSAGECOUNT];
   /*
    * Encode the MessageQueueItem over the wire
    */
@@ -65,7 +94,13 @@ private:
    * init to MAXRETRIES
    * to be used in sendMessage
    */
-  MessageQueueItem * getUnusedMessage();
+  bool getUnusedMessage(MessageQueueItem * item,  MessageQueueItem * queue);
+  /* 
+   * find the message which corresponds with the given parameters
+   * If more than one message matches the parameters, the first one is returned
+   * if a parameter is set to -1, this parameter is ignored
+   */
+  bool findMessage(int channel, int messageId, int retriesLeft, MessageQueueItem * item, MessageQueueItem * queue);
   uint8_t m_lastMessageId;
   MessageReceivedEventHandler callback;
 };
