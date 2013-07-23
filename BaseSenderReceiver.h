2bf665729fbac28a6d1d6f0797de54abf57ec36c
Bas Mesman <captainf@dds.nl>
2013-07-23 08:58:46 +0200

    added defaults for BaseSenderReceiver  methods return values

--- BaseSenderReceiver.h
+++ BaseSenderReceiver.h
@@ -8,9 +8,9 @@ class BaseSenderReceiver
 {
 public:
   BaseSenderReceiver();
-  virtual bool send(uint8_t * buf, uint8_t len){};
-  virtual bool have_message(){};
-  virtual bool get_message(uint8_t * buf, uint8_t * len){};  
+  virtual bool send(uint8_t * buf, uint8_t len){ return false;}
+  virtual bool have_message(){ return false;}
+  virtual bool get_message(uint8_t * buf, uint8_t * len){ return false;} 
 };
 
 
