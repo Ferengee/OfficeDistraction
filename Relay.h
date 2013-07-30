a788d6d071273717cecae6ab7876b8a79cb58c2d
Bas Mesman <captainf@dds.nl>
2013-07-30 22:31:24 +0200

    Simple relay class with on, off and toggle

--- Relay.h
+++ Relay.h
@@ -1,4 +1,18 @@
 #ifndef Relay_h
 #define Relay_h
+#include <Arduino.h>
 
+class Relay
+{
+public:
+  Relay(int commPin);
+  void toggle();
+  void on();
+  void off();
+  void update();
+  
+private:
+    int pin;
+    int state;
+};
 #endif
