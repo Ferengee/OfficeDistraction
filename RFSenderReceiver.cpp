b95c8b5440793fe318f0b6d2158754547e5237fa
Bas Mesman <captainf@dds.nl>
2013-08-11 12:24:50 +0200

    wrapped virtual wire

--- RFSenderReceiver.cpp
+++ RFSenderReceiver.cpp
@@ -10,3 +10,19 @@ RFSenderReceiver::RFSenderReceiver(int txPin, int rxPin, int speed)
     vw_rx_start(); 
   } 
 }
+
+bool RFSenderReceiver::get_message(uint8_t* buf, uint8_t* len)
+{
+  return vw_get_message(buf, len);
+}
+
+bool RFSenderReceiver::have_message()
+{
+  return vw_have_message();
+}
+
+bool RFSenderReceiver::send(uint8_t* buf, uint8_t len)
+{
+  return vw_send(buf, len);
+}
+
