0621104bdb7670f50c648fe3a5d0d82f67cab38f
Bas Mesman <captainf@dds.nl>
2013-08-16 20:45:04 +0200

    Made test receiver work as a pair. Added debugging output.

--- BaseSenderReceiver.h
+++ BaseSenderReceiver.h
@@ -21,11 +21,16 @@ public:
   TestSenderReceiver();
   virtual bool send(uint8_t * buf, uint8_t len);
   virtual bool have_message();
-  virtual bool get_message(uint8_t * buf, uint8_t * len);     
+  virtual bool get_message(uint8_t * buf, uint8_t * len);   
+  virtual bool receive(uint8_t * buf, uint8_t len);
+  TestSenderReceiver * m_other;
 private:
   MessageQueueItem m_buffer[MAXMESSAGECOUNT];
   int m_start;
   int m_end;
+  int m_sendcount;
+  
+  
 };
 
 #endif
